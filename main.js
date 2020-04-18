c376f522512fd7e73071ffe27615d0218318ce95
Juan <aragonlopezjuan87@gmail.com>
2020-04-18 15:42:21 +0200

    delate code main.js

--- main.js
+++ main.js
@@ -2,13 +2,7 @@ let name, age, city, profession, gender, checkedGender, $showData
 
 // class new user
 class NewUser{
-	constructor(name, age, city, profession, gender){
-		this.name = name,
-		this.age= age,
-		this.city = city,
-		this.profession = profession,
-		this.gender = gender
-	}
+	
 	handleGetNewUser(){
 		name= document.querySelector('#name').value
 		age = document.querySelector('#age').value
@@ -37,65 +31,15 @@ class NewUser{
 	}
 	showDataUI(name, age, city, profession,checkedGender){
 		$showData = document.getElementById("texto");
-		$showData.innerHTML = `Hola soy un ${checkedGender}, y mi nombre es ${name}. Tengo una edad de ${age} años. Soy ${profession} y vivo en ${city}` ;
+		$showData.innerHTML = `Hola soy un ${checkedGender}, y mi nombre es ${name}. Tengo ${age} años. Soy ${profession} y vivo en ${city}.` ;
 	}
 }
-
-
-
-
-
-
-
-
-var s, n, e, p, c, b, tsh, tsm, tn, te, tp, tc;
-
-/*tsh = document.getElementById("hombre");
-tsm = document.getElementById("mujer");
-var tsmv = (tsh.value);
-function hombreMujer() {
-	
-}*/
-
-tn = document.getElementById("nombre");
-var tnv = tn.value;  /////////////introducir los valores
-
-// function humano(s,n,e,p,c)
-// {
-// 	this.sexo = s;
-// 	this.nombre = n;
-// 	this.edad = e;
-// 	this.profecion = p;
-// 	this.ciudad = c;
-// };
-
-function genero() 
-{
-	var t = document.getElementById("texto");
-	s = hombre.sexo;
-	n = hombre.nombre;
-	e = hombre.edad;
-	p = hombre.profecion;
-	c = hombre.ciudad;
-	if (hombre.sexo == "hombre") 
-	{
-		t.innerHTML = "Hola soy un hombre, y mi nombre es " + n + ". Tengo una edad de " + e +" años. Me dedico profesionalmente a "+p+ " y soy de " +c ;
-	}
-	else
-	{
-		t.innerHTML = "Hola soy una mujer, y mi nombre es " + n + ". Tengo una edad de " + e +" años. Me dedico profesionalmente a "+p+ " y soy de " +c;
-	};
-};
 
 
 function inicio() 
 {
 	b = document.getElementById("Enviar");
 	b.addEventListener("click", app);
-	
-
-	//var hombre = new humano("mujer","pepe",28,"pescadero","cadiz");
-	
 };
 
 function app() 
